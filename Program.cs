4f7064d7c2b851e27e250d72e7bd9d9ddeeb5ac9
Anastasia <anastasia.serous@gmail.com>
2022-11-14 21:37:28 +0300

    Add program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
+*/
+
+string[] ScanStringArray(string[] array, int n)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n) result++;
+    }
+    string[] resultString = new string[result];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            resultString[j] = array[i];
+            j++;
+        }
+    }
+    return resultString;
+}
+
+string[] NewStringArray(int n)
+{
+    string[] result = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент: ");
+        result[i] = Console.ReadLine();
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[" + string.Join(", ", array) + "]");
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов в массиве: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] result = ScanStringArray(NewStringArray(num), 3);
+Console.Write ("Массив элементов меньше или равных 3 символам: ");
+PrintArray(result);
